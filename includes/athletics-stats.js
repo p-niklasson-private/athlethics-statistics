be8bd9b0c2d3e85d4b0028f9bf58f9a7c4031174
Patrik Niklasson <patrik@niklassons.net>
2016-12-06 22:56:54 +0000

    Improved the menu handling and started handling of parameters in the URL

--- includes/athletics-stats.js
+++ includes/athletics-stats.js
@@ -51,3 +51,100 @@ function findGetParameter(parameterName) {
     }
     return result;
 }
+
+function menu () {
+    var option_1 = '';  // Resultat
+    var option_2 = '';  // Personbästa
+    var option_3 = '';  // Statistik
+    var option_31 = ''; // Trender
+    var option_32 = ''; // Födelseår
+    var option_33 = ''; // Grenar
+    var option_34 = ''; // Tävlingar
+    var option_4 = '';  // Kartor
+    var option_41 = ''; // Inomhus 2016-2017
+    var option_42 = ''; // Utomhus 2016
+    var active = 'class="w3-green"';
+    
+    for (i = 0; i < arguments.length; i++) {
+        if (arguments[i] == '1') { option_1 = active; }
+        if (arguments[i] == '2') { option_2 = active; }
+        if (arguments[i] == '3') { option_3 = active; }
+        if (arguments[i] == '31') { option_31 = active; }
+        if (arguments[i] == '32') { option_32 = active; }
+        if (arguments[i] == '33') { option_33 = active; }
+        if (arguments[i] == '34') { option_34 = active; }
+        if (arguments[i] == '4') { option_4 = active; }
+        if (arguments[i] == '41') { option_41 = active; }
+        if (arguments[i] == '42') { option_42 = active; }
+    }  
+      
+    var menuString = 
+        '<ul class="w3-navbar w3-round-large w3-light-grey w3-medium w3-margin">' +
+        '<li><a ' + option_1 + 'href="index.html"><i class="fa fa-table"></i> Resultat</a></li>' +
+        '<li><a ' + option_2 + 'href="pb.html"><i class="fa fa-table"></i> Personbästa</a></li>' +
+        '<li class="w3-dropdown-hover">' +
+          '<a ' + option_3 + ' href="#"><i class="fa fa-bar-chart"></i> Statistik <i class="fa fa-caret-down"></i></a>' +
+          '<div class="w3-dropdown-content w3-white w3-card-4">' +
+            '<a ' + option_31 + 'href="trends.html"><i class="fa fa-line-chart"></i> Resultat, trender per tävlande och gren</a>' +
+            '<a ' + option_32 + 'href="stats.html?graph=1"><i class="fa fa-pie-chart"></i> Födelseår</a>' +
+            '<a ' + option_33 + 'href="stats.html?graph=2"><i class="fa fa-pie-chart"></i> Populära grenar</a>' +
+            '<a ' + option_34 + 'href="stats.html?graph=3"><i class="fa fa-pie-chart"></i> Populära tävlingar</a>' +
+          '</div>' +
+        '</li>' +    
+        '<li class="w3-dropdown-hover">' +
+          '<a ' + option_4 + 'href="#"><i class="fa fa-map"></i> Kartor <i class="fa fa-caret-down"></i></a>' +
+          '<div class="w3-dropdown-content w3-white w3-card-4">' +
+            '<a ' + option_41 + 'href="map.html?map=1"><i class="fa fa-map-marker"></i> Inomhustävlingar 2016-2017</a>' +
+            '<a ' + option_42 + 'href="map.html?map=2"><i class="fa fa-map-marker"></i> Utomhustävlingar 2016</a>' +
+          '</div>' +
+        '</li>' +
+        '<li class="w3-dropdown-hover">' +
+          '<a href="#"><i class="fa fa-link"></i> Länkar <i class="fa fa-caret-down"></i></a>' +
+          '<div class="w3-dropdown-content w3-white w3-card-4">' +
+            '<a href="https://hanvikenssk.myclub.se/friidrott" target="_blank">Hanviken SK friidrott</a>' +
+            '<a href="http://friidrott.se" target="_blank">Friidrott.se</a>' +
+          '</div>' +
+        '</li>' +
+      '</ul>';
+    document.getElementById('menu').innerHTML = menuString;
+}
+
+function updateUrl(url,key,value){
+      if(value!=undefined){
+        value = encodeURI(value);
+      }
+      var urls = url.split('?');
+      var baseUrl = urls[0];
+      var parameters = '';
+      var outPara = {};
+      if(urls.length>1){
+          parameters = urls[1];
+      }
+      if(parameters!=''){
+        parameters = parameters.split('&');
+        for(k in parameters){
+          var keyVal = parameters[k];
+          keyVal = keyVal.split('=');
+          var ekey = keyVal[0];
+          var eval = '';
+          if(keyVal.length>1){
+              eval = keyVal[1];
+          }
+          outPara[ekey] = eval;
+        }
+      }
+      if(value!=undefined){
+        outPara[key] = value;
+      }else{
+        delete outPara[key];
+      }
+      parameters = [];
+      for(var k in outPara){
+        parameters.push(k + '=' + outPara[k]);
+      }
+      var finalUrl = baseUrl;
+      if(parameters.length>0){
+        finalUrl += '?' + parameters.join('&'); 
+      }
+      return finalUrl; 
+  }
