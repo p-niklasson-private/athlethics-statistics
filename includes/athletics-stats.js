5a3c263891104f70187fd986918d8cf7c27069c4
Patrik Niklasson <patrik@niklassons.net>
2018-04-30 21:57:13 +0000

    Remove unused files and code

--- includes/athletics-stats.js
+++ includes/athletics-stats.js
@@ -1,6 +1,5 @@
 function initiateData() {
     dbGetJsonData();
-    //sessionGetJsonData();
 }
 
 function getData(record){
@@ -58,31 +57,6 @@ function getData(record){
             console.log(docList[i]);
         }
     }
-}
-
-/**
- * Store (cache) statistics data in browsers Session Storage
- *
- * @param   {number} a the first number
- * @param   {number} b the second number
- *
- * @returns {number} the sum of a and b
- */
-
-function sessionStoreJsonData() {
-    // Store the json data in local sessionStorage
-    var today = new Date().toJSON().slice(0,10);
-    if (sessionStorage) {
-        console.log("Storing new data for '" + docs + "' in sessionStorage: " + today)
-        sessionStorage.setItem(docs, data.toJSON());
-        sessionStorage.setItem("date_" + docs, today);
-    }
-}
-
-function sessionGetJsonData(){
-    // Get json data from local sessionStorage
-    record = {data: sessionStorage.getItem(docs), date: sessionStorage.getItem("date_" + docs)};
-    getData(record);
 }
 
 function dbStoreJsonData() {
@@ -267,88 +241,6 @@ function storeFilter(filter, name) {
         bookmarkUrl = updateUrl(bookmarkUrl,name,values);
         console.log("bookmarkUrl: " + bookmarkUrl);
         $("#permaLink").attr("href", bookmarkUrl);
-    }
-}
-
-function populateEventSelections() {
-    var eventNo = eventForm.eventList.selectedIndex;
-    var eventSelectionList=document.getElementById('eventList');
-    eventSelectionList.innerHTML = '';
-    var events = view.getDistinctValues(0); // Column 0 = events
-    eventSelectionList.add(new Option("- Alla grenar -", 'all'));
-    for(var i = 0; i < events.length; i++) {
-        eventSelectionList.add(new Option(events[i], events[i]));
-    }
-    if (eventSelectionList.length == 2){
-        if (eventNo) eventSelectionList.options.selectedIndex = 1;
-        else eventSelectionList.options.selectedIndex = 0;
-    }
-}
-
-function populateNameSelections() {
-    var nameNo = nameForm.nameList.selectedIndex;
-    var nameSelectionList=document.getElementById('nameList');
-    nameSelectionList.innerHTML = '';
-    var names = view.getDistinctValues(1); // Column 1 = names
-    nameSelectionList.add(new Option("- Alla namn -", 'all'));
-    for(var i = 0; i < names.length; i++) {
-        nameSelectionList.add(new Option(names[i], names[i]));
-    }
-    if (nameSelectionList.length == 2){
-        if (nameNo) nameSelectionList.options.selectedIndex = 1;
-        else nameSelectionList.options.selectedIndex = 0;
-    }
-}
-
-function populateYearSelections() {
-    var yearNo = yearForm.yearList.selectedIndex;
-    var yearSelectionList=document.getElementById('yearList');
-    yearSelectionList.innerHTML = '';
-    var years = view.getDistinctValues(2).reverse();
-    
-    yearSelectionList.add(new Option("- Alla födelseår -", 'all'));
-    for(var i = 0; i < years.length; i++) {
-        yearSelectionList.add(new Option(years[i], years[i]));
-    }
-    if (yearSelectionList.length == 2){
-        if (yearNo) yearSelectionList.options.selectedIndex = 1;
-        else yearSelectionList.options.selectedIndex = 0;
-    }
-}
-
-function populateClassSelections(column) {
-    if (! column) { 
-        column = 8;
-    }
-    var classNo = classForm.classList.selectedIndex;
-    var classSelectionList=document.getElementById('classList');
-    classSelectionList.innerHTML = '';
-    var classes = view.getDistinctValues(column);
-    classSelectionList.add(new Option("- Alla klasser -", 'all'));
-    for(var i = 0; i < classes.length; i++) {
-        classSelectionList.add(new Option(classes[i], classes[i]));
-    }
-    if (classSelectionList.length == 2){
-        if (classNo) classSelectionList.options.selectedIndex = 1;
-        else classSelectionList.options.selectedIndex = 0;
-    }
-}
-
-function populateSeasonSelections(column) {
-    if (! column) { 
-        column = 12;
-    }
-    var seasonNo = seasonForm.seasonList.selectedIndex;
-    var seasonSelectionList=document.getElementById('seasonList');
-    seasonSelectionList.innerHTML = '';
-    var seasons = view.getDistinctValues(column);
-    seasonSelectionList.add(new Option("- Alla säsonger -", 'all'));
-    for(var i = 0; i < seasons.length; i++) {
-        seasonSelectionList.add(new Option(seasons[i], seasons[i]));
-    }
-    if (seasonSelectionList.length == 2){
-        if (seasonNo) seasonSelectionList.options.selectedIndex = 1;
-        else seasonSelectionList.options.selectedIndex = 0;
     }
 }
 
--- includes/readHtmlFile.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if (isset($_GET['url'])) {
-        $string = file_get_contents($_GET['url']);
-        echo $string;
-    }
-?>
