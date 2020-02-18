aa2b00ecccbc7a8113231c5bb054eb009bdfab82
Patrik Niklasson <patrik@niklassons.net>
2020-02-18 12:42:48 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-cp /www/git/development/athletics-stats/data/Data.json /www/git/development/athletics-stats/data/Data.json
+cp /www/git/development/athletics-stats/data/Data.json /www/git/production/athletics-stats/data/Data.json
 git pull
 cache-purge hanviken.niklassons.net
 cache-purge hanviken-dev.niklassons.net
